71f6314796931d07409cec33f8971418f753f4a5
Pakin49 <philgus884@gmail.com>
2025-06-21 23:47:16 +0700

    remove scrollwheel change tag

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -153,11 +153,11 @@ awful.util.taglist_buttons = mytable.join(
 		if client.focus then
 			client.focus:move_to_tag(t)
 		end
-	end),
-	awful.button({}, 3, awful.tag.viewtoggle),
+	end)
+	--[[awful.button({}, 3, awful.tag.viewtoggle),
 	awful.button({ modkey }, 3, function(t)
 		if client.focus then
-			client.focus:toggle_tag(t)
+		awful	client.focus:toggle_tag(t)
 		end
 	end),
 	awful.button({}, 4, function(t)
@@ -165,7 +165,7 @@ awful.util.taglist_buttons = mytable.join(
 	end),
 	awful.button({}, 5, function(t)
 		awful.tag.viewprev(t.screen)
-	end)
+	end) --]]
 )
 
 awful.util.tasklist_buttons = mytable.join(
