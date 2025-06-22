5c503f3cd6d57f7c24eb1aa4416a1518432fab6e
Pakin49 <philgus884@gmail.com>
2025-06-22 14:00:53 +0700

    darker

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -903,7 +903,7 @@ require('lazy').setup({
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require('onedark').setup {
-        style = 'dark',
+        style = 'darker',
         transparent = true,
       }
       -- Enable theme
