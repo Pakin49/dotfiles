fe6a4ace8c6867d14d1a046fe9745fd070f00c87
Pakin49 <philgus884@gmail.com>
2025-08-04 20:47:13 +0700

    .

--- Templates/scripts/force_tile.sh
+++ Templates/scripts/force_tile.sh
@@ -1,9 +1,12 @@
 #!/bin/zsh
 
 state=$(grep 'force tiling' ~/.config/awesome/rc.lua)
-if [ $"state"='--[[ force tiling' ]
+echo $state
+if [[ "$state" == '--[[ force tiling' ]]
 then
         sed -i 's/--\[\[ force tiling/-- \[\[ force tiling/' ~/.config/awesome/rc.lua
+        echo "True"
 else
         sed -i 's/-- \[\[ force tiling/--\[\[ force tiling/' ~/.config/awesome/rc.lua
+        echo "False"
 fi
