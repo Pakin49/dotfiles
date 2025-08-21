4e4e0f4de73305b3ef744ec74b8693b6494f0588
Pakin49 <philgus884@gmail.com>
2025-08-21 19:41:59 +0700

    .

--- .config/nvim/lua/config/custom/transparent.lua
+++ .config/nvim/lua/config/custom/transparent.lua
@@ -1,27 +1,49 @@
--- Add transparency
-local M = {}
+local m = {}
 
+-- save the current theme value
+local normal = vim.api.nvim_get_hl(0, { name = "normal" })
+local normal_nc = vim.api.nvim_get_hl(0, { name = "normalnc" })
+local end_of_buffer = vim.api.nvim_get_hl(0, { name = "endofbuffer" })
 
---vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
---vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
---vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+local home_dir = os.getenv("HOME")
+local file_path = home_dir .. "/.local/state/nvim/custom/transparent.txt"
 
-local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
-local normal_nc = vim.api.nvim_get_hl(0, { name = "NormalNC" })
-local end_of_buffer = vim.api.nvim_get_hl(0, { name = "EndOfBuffer" })
-
-M.transparents_enabled = true
-function M.toggle_transparent()
-  if M.transparents_enabled then
-    vim.api.nvim_set_hl(0, "Normal", { bg = normal.bg })
-    vim.api.nvim_set_hl(0, "NormalNC", { bg = normal_nc.bg})
-    vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = end_of_buffer.bg })
-    M.transparents_enabled = false
-  else
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-    vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
-    M.transparents_enabled = true
-  end
+local get_transparent_state = function()
+	local file = io.open(file_path, "r")
+	if file then
+		m.transparents_enabled = file:read()
+	else
+		os.execute("mkdir -p " .. home_dir .. "/.local/state/custom/nvim  && echo 'false' > " .. file_path)
+		m.transparents_enabled = false
+	end
 end
-return M
+
+local save_transparent_state = function()
+	local file = io.open(file_path, "w")
+	file:write(tostring(m.transparents_enabled))
+  file:close()
+end
+
+function m.toggle_transparent()
+	if m.transparents_enabled then
+		vim.api.nvim_set_hl(0, "normal", { bg = normal.bg })
+		vim.api.nvim_set_hl(0, "normalnc", { bg = normal_nc.bg })
+		vim.api.nvim_set_hl(0, "endofbuffer", { bg = end_of_buffer.bg })
+		m.transparents_enabled = false
+	else
+		vim.api.nvim_set_hl(0, "normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "normalnc", { bg = "none" })
+		vim.api.nvim_set_hl(0, "endofbuffer", { bg = "none" })
+		m.transparents_enabled = true
+	end
+	save_transparent_state()
+end
+
+get_transparent_state()
+if m.transparents_enabled == true then
+	vim.api.nvim_set_hl(0, "normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "normalnc", { bg = "none" })
+	vim.api.nvim_set_hl(0, "endofbuffer", { bg = "none" })
+	m.transparents_enabled = true
+end
+return m
--- .config/nvim/test.lua
+++ .config/nvim/test.lua
@@ -1,2 +1,13 @@
-local f = io.open(vim.fn.stdpath("custom").."/transparent_flag","w")
-f:write(tostring(
+local file = io.open("~/.local/state/nvim/custom/transparent.txt","r")
+local transparent_state = false
+if file then
+  transparent_state = file:read()
+else
+  os.execute("mkdir -p ~/local/state/nvim/custom && 'false' > transparent.txt")
+end
+
+local save_transparent_state = function () 
+  local file = io.open("~/.local/state/nvim/custom/transparent.txt","w")
+  file:write()
+
+end 
