39270662ace55b0acc2bc158e8c61b10db41f708
Pakin49 <philgus884@gmail.com>
2025-09-09 20:03:21 +0700

    done count uncategorized pkg

--- packages/check_pkg.c
+++ packages/check_pkg.c
@@ -2,56 +2,95 @@
 #include <stdio.h>
 #include <string.h>
 
+#define PKG_LENGHT 100
 
 bool check_pkg_format(char *str) {
+  bool valid = false;
   while (*str) {
     if (*str == '#' || *str == '(' || *str == ')')
       return false;
+    if (*str != ' ' && *str != '\n')
+      valid = true;
     str++;
   }
-  return true;
+  return valid;
 }
 
-bool categorized(char *pkg, char (*cpkg_list)[100] ){
-  // check if pkg is already categorized or not?
-  return true; 
+bool check_categorized(char *pkg, char (*cpkg_list)[PKG_LENGHT], int count) {
+  pkg[strcspn(pkg, "\n")] = '\0';
+  for (int i = 0; i < count; i++) {
+    if (strcmp(pkg, *cpkg_list) == 0) {
+      return true;
+    }
+    cpkg_list++;
+  }
+  return false;
 }
 
+void extract_name(char *str) {
+  char pkg_name[PKG_LENGHT] = "";
+  char *ptr = str;
+  int i = 0;
+  while (*ptr != '\n') {
+    if (*ptr == ' ') {
+      ptr++;
+      continue;
+    }
+    pkg_name[i] = *ptr;
+    i++;
+    ptr++;
+  }
+  strcpy(str, pkg_name);
+}
 int main(int argc, char **argv) {
   const int NUMS_PACKAGE = 300;
+  int count_cpkg = 0;
   if (argc != 3) {
-    printf("Incorrect usage: ./check_pkg <pkg_list> <editting file>");
+    printf("Incorrect usage: ./check_pkg <categorized_pkg_list> <pkg_list>");
     return -1;
   }
-  char buffer[100];
+  char buffer[PKG_LENGHT];
 
   // this is file that we are editing and try to categorize pkgs
-  printf("----------Categorized packges ----------\n");
+  //printf("----------Categorized packges ----------\n");
   FILE *fptr0;
   fptr0 = fopen(argv[1], "r");
-  char categorized_pkg_list[NUMS_PACKAGE][100];
-  char (*cpkg_ptr)[100] = categorized_pkg_list; 
-  while (fgets(buffer, 100, fptr0)){
-    if (!check_pkg_format(buffer))
-      printf("yes\n");//printf("%s", buffer);
-    cpkg_ptr++;
+  if (!fptr0) {
+    printf("Invalid categorized_pkg_list\n");
+    return -1;
   }
-  printf("---------------------------------------\n");
-  
+  char categorized_pkg_list[NUMS_PACKAGE][PKG_LENGHT];
+  char (*cpkg_ptr)[PKG_LENGHT] = categorized_pkg_list;
+  while (fgets(buffer, PKG_LENGHT, fptr0)) {
+    if (check_pkg_format(buffer)) {
+      extract_name(buffer);
+      strcpy(*cpkg_ptr, buffer);
+      cpkg_ptr++;
+      count_cpkg++;
+    }
+  }
+  //printf("%d packages are categorized\n",count_cpkg);
+  //printf("---------------------------------------\n");
+  fclose(fptr0);
+
   // Save the pkg in an arrays
   FILE *fptr1;
+  int count_pkg = 0;
   fptr1 = fopen(argv[2], "r");
-  char pkg_list[NUMS_PACKAGE][100];
-  char (*pkg_ptr)[100] = pkg_list;
-  printf("---------- List of package ----------\n");
-  while (fgets(buffer, 100, fptr1)) {
-    strcpy(*pkg_ptr, buffer);
-    printf("%s", *pkg_ptr);
-    pkg_ptr++;
+  if (!fptr1) {
+    printf("Invalid categorized_pkg_list\n");
+    return -1;
   }
-  printf("-------------------------------------\n");
+  printf("---------- List of uncategorized package ----------\n");
+  while (fgets(buffer, PKG_LENGHT, fptr1)) {
+    if (!check_categorized(buffer, categorized_pkg_list, count_cpkg)) {
+      printf("Not categorized : %s\n", buffer);
+      count_pkg++;
+    }
+  }
+  printf("%d packages are uncategorized\n",count_pkg);
+  printf("---------------------------------------------------\n");
 
-  fclose(fptr0);
   fclose(fptr1);
   return 0;
 }
