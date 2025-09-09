3daba2ce30f8d9a3c64bb291259221830343509e
Pakin49 <pakin.pan@proton.me>
2025-09-09 09:04:39 +0700

    Remove the betterlockscreen wallpaper

--- .config/rofi/powermenu/type-4/powermenu.sh
+++ .config/rofi/powermenu/type-4/powermenu.sh
@@ -93,7 +93,7 @@ case ${chosen} in
         ;;
     $lock)
 		if [[ -x '/usr/bin/betterlockscreen' ]]; then
-			betterlockscreen -l -u ~/Pictures/wallpapers/
+			betterlockscreen -l 
 		elif [[ -x '/usr/bin/i3lock' ]]; then
 			i3lock
 		fi
