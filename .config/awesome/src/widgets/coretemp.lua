c863eb2b2f2700f6b9166f1dee128dd57370504c
Pakin49 <pakin.pan@proton.me>
2025-10-08 01:49:56 +0700

    sadge ;-;, awesome move to archive repo use pop as main DE

--- .config/awesome/src/widgets/coretemp.lua
+++
@@ -1,7 +0,0 @@
--- Coretemp
-local tempicon = wibox.widget.imagebox(theme.widget_temp)
-local temp = lain.widget.temp({
-	settings = function()
-		widget:set_markup(markup.font(theme.font, " " .. coretemp_now .. "°C "))
-	end,
-})
